ef02e67008f2c8cc69f83b43ac90a771b5dfda2c
ProstoMokaksin <prosto.mokasin@gmail.com>
2017-02-25 23:03:22 +0000

    Update videos.js

--- videos.js
+++ videos.js
@@ -3,6 +3,6 @@ const videos = {
 	"net": { id: "ayq41-UJ7Cg", start: "0", name: "Look At This Net" },
 	"timotei": { id: "b5KH9Z383hE", start: "0", name: "Timotei" },
 	"mariopingpong": { id: "sMKoNBRZM1M", start: "6", name: "Mario Ping Pong" },
-	"louder": { id: "GJ_SHqbtlRY", start: "0", name: "Louder", }
+	"louder": { id: "GJ_SHqbtlRY", start: "0", name: "Louder" },
 	"yeahboi": { id: "fvtQYsckLxk", start: "64", name: "Yeah Boi" }
 }
