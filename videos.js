21ffafa40be183ca037b8b6d63cd012d8ddc33b4
Maxim Deprez <mxmcube@gmail.com>
2017-02-25 22:52:45 +0000

    First upload

---
+++ videos.js
@@ -0,0 +1,7 @@
+const videos = {
+	"katletka": { id: "A1Qb4zfurA8", start: "16", name: "Katletka" },
+	"net": { id: "ayq41-UJ7Cg", start: "0", name: "Look At This Net" },
+	"timotei": { id: "b5KH9Z383hE", start: "0", name: "Timotei" },
+	"mariopingpong": { id: "sMKoNBRZM1M", start: "6", name: "Mario Ping Pong" },
+	"louder": { id: "GJ_SHqbtlRY", start: "0", name: "Louder" }
+}
