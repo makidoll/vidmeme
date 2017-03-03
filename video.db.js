10d8326905bb36e108048e045758b99df5196162
Maxim Deprez <mxmcube@gmail.com>
2017-03-03 20:06:57 +0000

    Overall site improvement. Working on an autoplay feature.

--- video.db.js
+++ video.db.js
@@ -1,4 +1,4 @@
-{
+var videos = {
 	"katletka": { "id": "A1Qb4zfurA8", "start": "16", "name": "Katletka" },
 	"net": { "id": "ayq41-UJ7Cg", "start": "0", "name": "Look At This Net" },
 	"timotei": { "id": "b5KH9Z383hE", "start": "0", "name": "Timotei" },
@@ -6,9 +6,8 @@
 	"louder": { "id": "GJ_SHqbtlRY", "start": "0", "name": "Louder" },
 	"yeahboi": { "id": "fvtQYsckLxk", "start": "65", "name": "Yeah Boi" },
 	"leekspin": { "id": "1wnE4vF9CQ4", "start": "0", "name": "Leek Spin" },
-	"nullpointer": { "id": "bLHL75H_VEM", "start": "0", "name": "Null Pointer" },
 	"blyaaaat": { "id": "9nQ-PXHV6nw", "start": "4", "name": "BLYAAAAT" },
-	"ppap": { "id": "0E00Zuayv9Q", "start": "15", "name": "PPAP" },
+	"ppap": { "id": "0E00Zuayv9Q", "start": "13", "name": "PPAP" },
 	"catflower": { "id": "-Z4jx5VMw8M", "start": "9", "name": "Can't Handle Flower" },
 	"hihowareya": { "id": "ewKZStzQOYc", "start": "0", "name": "Hi How Are Ya" }
 }
